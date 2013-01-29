f314aa151023fa31f1dd84e4638fa2072dcaeed4
Shon M <him@shonm.com>
2013-01-28 21:40:22 -0700

    EmptyJob should say whether it's throwing or not

--- Jobs/EmptyJob.php
+++ Jobs/EmptyJob.php
@@ -8,10 +8,11 @@ class EmptyJob extends ContainerAwareJob
 {
     public function perform ()
     {
-        fwrite(STDOUT, "\x1B[31m" . 'Performing a job - Hello from EmptyJob!' . "\x1B[39m" . "\n");
-
         if ($this->args['fail']) {
+            fwrite(STDOUT, "\x1B[31m" . 'Throwing a job - Goodbye from EmptyJob!' . "\x1B[39m" . "\n");
             throw new \Exception("This is a completely empty exception");
         }
+
+        fwrite(STDOUT, "\x1B[31m" . 'Performing a job - Hello from EmptyJob!' . "\x1B[39m" . "\n");
     }
 }
