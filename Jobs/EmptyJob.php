8864a131266847f4a46d7855932bea81463c6db6
Shon M <him@shonm.com>
2013-01-28 21:49:41 -0700

    EmptyJob: Undefined index. Dependency updates

--- Jobs/EmptyJob.php
+++ Jobs/EmptyJob.php
@@ -8,7 +8,7 @@ class EmptyJob extends ContainerAwareJob
 {
     public function perform ()
     {
-        if ($this->args['fail']) {
+        if (! empty($this->args['fail'])) {
             fwrite(STDOUT, "\x1B[31m" . 'Throwing a job - Goodbye from EmptyJob!' . "\x1B[39m" . "\n");
             throw new \Exception("This is a completely empty exception");
         }
