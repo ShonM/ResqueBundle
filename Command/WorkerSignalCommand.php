0997810550c5cb8002799167cc6bff172ef7d514
Shon M <him@shonm.com>
2013-03-30 16:47:57 -0600

    Worker restart -> signal command. "Restart" is subjective.

--- Command/WorkerSignalCommand.php
+++ Command/WorkerSignalCommand.php
@@ -9,17 +9,19 @@ use Symfony\Component\Console\Input\InputOption,
     Symfony\Component\Process\Process,
     Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand,
     ShonM\ResqueBundle\Worker,
+    InvalidArgumentException,
     RuntimeException,
     Resque_Worker;
 
-class WorkerRestartCommand extends ContainerAwareCommand
+class WorkerSignalCommand extends ContainerAwareCommand
 {
     protected function configure()
     {
-        $this->setName('resque:worker:restart')
-             ->setDescription('Restart workers')
+        $this->setName('resque:worker:signal')
+             ->setDescription('Signal workers (shutdown, restart, pause, and resume)')
              ->addArgument('worker', InputArgument::OPTIONAL, 'Worker name')
-             ->addOption('all', null, InputOption::VALUE_NONE, 'Restart all workers')
+             ->addOption('signal', null, InputOption::VALUE_OPTIONAL, 'Signal [QUIT|TERM|INT|USR1|USR2|CONT]', 'QUIT')
+             ->addOption('all', null, InputOption::VALUE_NONE, 'Signal all workers')
         ;
     }
 
@@ -35,31 +37,50 @@ class WorkerRestartCommand extends ContainerAwareCommand
         // Now find all existing workers
         $workers = $resque->workers();
 
+        if (! $workers) {
+            return $output->writeln('<error>There are no available workers</error>');
+        }
+
+        $descriptions = array(
+            'QUIT'     => 'Wait for child to finish processing then exit',
+            'TERM|INT' => 'Immediately kill child then exit',
+            'USR1'     => 'Immediately kill child but don\'t exit',
+            'USR2'     => 'Pause worker, no new jobs will be processed',
+            'CONT'     => 'Resume worker',
+        );
+
+        if (! in_array($input->getOption('signal'), array_keys($descriptions))) {
+            throw new InvalidArgumentException('The signal "' . $input->getOption('signal') . '" is not in the list of available signals');
+        }
+
+        $output->writeln('Signal: <comment>' . $input->getOption('signal') . '</comment>');
+        $output->writeln('');
+
         // If there was a specific worker passed, check for it
         if ($target = $input->getArgument('worker')) {
             foreach ($workers as $worker) {
                 if ($target == (string) $worker) {
-                    return $this->restart($worker);
+                    return $this->signal($worker, $input->getOption('signal'));
                 }
             }
 
             return $output->writeln('<error>The worker specified could not be found</error>');
         }
 
-        // If the all option was passed, just straight up restart everything
+        // If the all option was passed, just straight up signal everything
         if ($input->getOption('all')) {
             foreach ($workers as $worker) {
-                $this->restart($worker);
+                $this->signal($worker, $input->getOption('signal'));
             }
         } else {
             // Check if dialog helper supports choices, which makes things much easier for the user
             $dialog = $this->getHelper('dialog');
             if (method_exists($dialog, 'select')) {
-                $pick = $dialog->select($output, 'Select a worker to restart:', $workers);
-                $this->restart($workers[$pick]);
+                $pick = $dialog->select($output, 'Select a worker to signal:', $workers);
+                $this->signal($workers[$pick], $input->getOption('signal'));
             } else {
                 // Fall back to copy paste :(
-                $output->writeln('Active workers (copy and paste to restart):');
+                $output->writeln('Active workers (copy and paste to signal):');
                 foreach ($workers as $worker) {
                     $output->writeln('    ' . $worker);
                 }
@@ -67,21 +88,17 @@ class WorkerRestartCommand extends ContainerAwareCommand
         }
     }
 
-    private function restart(Worker $worker)
+    private function signal(Worker $worker, $signal = 'QUIT')
     {
         list($machine, $process, $queue) = explode(':', $worker->getId());
 
-        $process = new Process('kill -QUIT ' . $process);
+        $process = new Process('kill -' . $signal . ' ' . $process);
         $process->run();
 
         if (! $process->isSuccessful()) {
             throw new RuntimeException($process->getErrorOutput());
         }
 
-        $this->output->writeln($worker->getId() . ' restarting');
-
-        $worker = $this->getContainer()->get('resque.worker_daemon');
-        $worker->defineQueue($queue);
-        $worker->daemonize();
+        $this->output->writeln($worker->getId() . ' signaled');
     }
 }
