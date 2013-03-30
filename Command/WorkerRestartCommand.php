6e2fb5b025e44efcb00b39e1ff2f32ac00cf327b
Shon M <him@shonm.com>
2013-03-30 16:28:03 -0600

    Worker restart command can show a list of workers (or choices, if supported)

--- Command/WorkerRestartCommand.php
+++ Command/WorkerRestartCommand.php
@@ -8,6 +8,7 @@ use Symfony\Component\Console\Input\InputOption,
     Symfony\Component\Console\Output\OutputInterface,
     Symfony\Component\Process\Process,
     Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand,
+    ShonM\ResqueBundle\Worker,
     RuntimeException,
     Resque_Worker;
 
@@ -17,11 +18,14 @@ class WorkerRestartCommand extends ContainerAwareCommand
     {
         $this->setName('resque:worker:restart')
              ->setDescription('Restart workers')
+             ->addArgument('worker', InputArgument::OPTIONAL, 'Worker name')
+             ->addOption('all', null, InputOption::VALUE_NONE, 'Restart all workers')
         ;
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
+        $this->output = $output;
         $resque = $this->getContainer()->get('resque');
 
         // Prune dead workers first
@@ -31,21 +35,53 @@ class WorkerRestartCommand extends ContainerAwareCommand
         // Now find all existing workers
         $workers = $resque->workers();
 
-        foreach ($workers as $worker) {
-            list($machine, $process, $queue) = explode(':', $worker->getId());
-
-            $process = new Process('kill -QUIT ' . $process);
-            $process->run();
-
-            if (! $process->isSuccessful()) {
-                throw new RuntimeException($process->getErrorOutput());
+        // If there was a specific worker passed, check for it
+        if ($target = $input->getArgument('worker')) {
+            foreach ($workers as $worker) {
+                if ($target == (string) $worker) {
+                    return $this->restart($worker);
+                }
             }
 
-            $output->writeln($worker->getId() . ' restarting');
-
-            $worker = $this->getContainer()->get('resque.worker_daemon');
-            $worker->defineQueue($queue);
-            $worker->daemonize();
+            return $output->writeln('<error>The worker specified could not be found</error>');
         }
+
+        // If the all option was passed, just straight up restart everything
+        if ($input->getOption('all')) {
+            foreach ($workers as $worker) {
+                $this->restart($worker);
+            }
+        } else {
+            // Check if dialog helper supports choices, which makes things much easier for the user
+            $dialog = $this->getHelper('dialog');
+            if (method_exists($dialog, 'select')) {
+                $pick = $dialog->select($output, 'Select a worker to restart:', $workers);
+                $this->restart($workers[$pick]);
+            } else {
+                // Fall back to copy paste :(
+                $output->writeln('Active workers (copy and paste to restart):');
+                foreach ($workers as $worker) {
+                    $output->writeln('    ' . $worker);
+                }
+            }
+        }
+    }
+
+    private function restart(Worker $worker)
+    {
+        list($machine, $process, $queue) = explode(':', $worker->getId());
+
+        $process = new Process('kill -QUIT ' . $process);
+        $process->run();
+
+        if (! $process->isSuccessful()) {
+            throw new RuntimeException($process->getErrorOutput());
+        }
+
+        $this->output->writeln($worker->getId() . ' restarting');
+
+        $worker = $this->getContainer()->get('resque.worker_daemon');
+        $worker->defineQueue($queue);
+        $worker->daemonize();
     }
 }
